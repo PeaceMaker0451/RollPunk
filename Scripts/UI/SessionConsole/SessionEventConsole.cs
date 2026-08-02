4e327d751057999b9665225c97ee2f446ea7da33
peacemkr_png <nikitatum2005@gmail.com>
2026-08-02 21:55:31 +0900

    написание event и event console

---
+++ Scripts/UI/SessionConsole/SessionEventConsole.cs
@@ -0,0 +1,50 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+
+namespace RollPunk.Scripts.UI.SessionConsole
+{
+    public partial class SessionEventConsole : Node
+    {
+        [Export] RichTextLabel _text;
+        
+        public void AddLog(Event log)
+        {
+            WriteEvent(log);
+        }
+
+        public void InsertLogCollection(ICollection<Event> logCollection)
+        {
+            _text.Clear();
+            
+            foreach (Event log in logCollection)
+                WriteEvent(log);
+        }
+
+        private void WriteEvent(Event log)
+        {
+            string errorPrefix = "[color=red]";
+            string userPrefix = "[color=cyan]";
+            string systemPrefix = "[color=yellow]";
+
+            string prefix = "";
+
+            switch(log.Type)
+            {
+                case SourceType.User:
+                    prefix = userPrefix;
+                    break;
+
+                case SourceType.System:
+                    prefix = systemPrefix;
+                    break;
+
+                case SourceType.Error:
+                    prefix = errorPrefix;
+                    break;
+            }
+
+            _text.AppendText($"[i][{log.Date.ToLocalTime().ToShortTimeString()}][/i] {prefix}[b]{log.Source}[/b][/color] - {log.Data}\n");
+        }
+    }
+}
