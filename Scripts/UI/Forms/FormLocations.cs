14b1649790d37e1b49883f3173a0cfe48e91bf6a
peacemkr_png <nikitatum2005@gmail.com>
2026-08-18 22:08:04 +0900

    добавил FileLocations

---
+++ Scripts/UI/Forms/FormLocations.cs
@@ -0,0 +1,12 @@
+﻿namespace RollPunk.UI.Forms
+{
+    /// <summary>
+    /// Где именно показана форма.
+    /// </summary>
+    public enum FormLocation
+    {
+        Closed,
+        NewWindow,
+        MainTab
+    }
+}
